a34930ab32a6af587c4bfae6c3427a5391853066
Tim Oliver <me@timoliver.com.au>
2019-04-05 00:29:39 +0900

    Implemented sample app logic

--- TOLocalServiceDiscoveryExample/ViewController.m
+++ TOLocalServiceDiscoveryExample/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "TOLocalServiceDiscovery.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) TOLocalServiceDiscovery *serviceDiscovery;
 
 @end
 
@@ -16,8 +19,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    __weak typeof(self) weakSelf = self;
+
+    NSArray *serviceTypes = @[@"_smb._tcp.",
+                              @"_ftp._tcp.",
+                              @"_sftp-ssh._tcp.",
+                              ];
+    self.serviceDiscovery = [[TOLocalServiceDiscovery alloc] initWithSearchServiceTypes:serviceTypes];
+    self.serviceDiscovery.serviceListUpdatedHandler = ^{
+        [weakSelf.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationFade];
+    };
+    [self.serviceDiscovery startDiscovery];
 }
 
+#pragma mark - Table View Delegate + Data Source -
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.serviceDiscovery.services.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+
+    NSNetService *service = self.serviceDiscovery.services[indexPath.row];
+    cell.textLabel.text = service.name;
+    cell.detailTextLabel.text = service.type;
+
+    return cell;
+}
 
 @end
