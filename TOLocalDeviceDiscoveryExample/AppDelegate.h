b1d1964156c24a55782c6c6d9e5d71cdc0df3168
Tim Oliver <me@timoliver.com.au>
2019-04-04 23:17:01 +0900

    Initial Commit

---
+++ TOLocalDeviceDiscoveryExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TOLocalDeviceDiscoveryExample
+//
+//  Created by Tim Oliver on 4/4/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
